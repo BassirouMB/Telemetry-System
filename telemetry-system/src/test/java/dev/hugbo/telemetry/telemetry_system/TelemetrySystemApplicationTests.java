66e73295a3a41ab7fb17fa9b15b794a7f9772e4c
Bassirou Matthías Mbaye <bassmb0906@gmail.com>
2025-09-10 13:00:07 +0000

    Setja upp Java Spring Boot

---
+++ telemetry-system/src/test/java/dev/hugbo/telemetry/telemetry_system/TelemetrySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hugbo.telemetry.telemetry_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetrySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
