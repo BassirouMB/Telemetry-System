dde01fc717116df1ce4dbd152f4b82ca101a9199
DanniFV <gitignore>
2025-09-25 19:07:40 +0000

    change password er núna PutMapping frekar en post, green -> orange

--- telemetry-system/src/main/java/dev/hugbo/telemetry/telemetry_system/controllers/UserController.java
+++ telemetry-system/src/main/java/dev/hugbo/telemetry/telemetry_system/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,7 @@ public class UserController {
         return userService.deleteUser(name);
     }
 
-    @PostMapping("/changePassword")
+    @PutMapping("/changePassword")
     public User changePassword(@RequestParam String name, @RequestParam String password, @RequestParam String newPassword) {
         return userService.changePassword(name, password, newPassword);
     }
